88f39ef2ea68dfb71e576eb148192e6c6a5e20e2
James Herndon <toska.admin@proton.me>
2026-04-15 05:34:38 +0000

    Basic Connection to LaserStream

--- src/config.rs
+++ src/config.rs
@@ -1,1 +1,47 @@
 //config.rs for env, or api keys, and other config data
+use dotenvy::dotenv;
+use std::{env, collections::HashMap};
+use helius_laserstream::{LaserstreamConfig, grpc::SubscribeRequest, grpc::SubscribeRequestFilterTransactions};
+
+#[derive(Debug)]
+pub struct AppConfig {
+    pub laserstream_config: LaserstreamConfig,
+    pub subscribe_request: SubscribeRequest,
+}
+
+impl AppConfig {
+    pub fn load() -> Result<Self, Box<dyn std::error::Error>> {
+        dotenv().ok();
+
+        let api_key = env::var("HELIUS_API_KEY")
+            .expect("HELIUS_API_KEY must be set in .env file");
+
+        let laserstream_config = LaserstreamConfig::new(
+            "https://laserstream-devnet-ewr.helius-rpc.com".to_string(),
+            api_key,
+        );
+
+        let mut transactions_filter = HashMap::new();
+        transactions_filter.insert(
+            "token-txs".to_string(),
+            SubscribeRequestFilterTransactions {
+                vote: Some(false),
+                failed: None,
+                account_include: vec![],
+                account_exclude: vec![],
+                account_required: vec![],
+                signature: None,
+            },
+        );
+
+        let subscribe_request = SubscribeRequest {
+            transactions: Some(transactions_filter).expect("Error in transactions_filter"),
+            ..Default::default()
+        };
+
+        Ok(AppConfig {
+            laserstream_config,
+            subscribe_request,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+use config::AppConfig;
+use helius_laserstream::{subscribe};
+use futures_util::StreamExt;
+use tracing_subscriber;
+use tokio::signal;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+    
+    let config = AppConfig::load()?;
+    
+    println!("Starting indexer..");
+
+    let (stream, _handle) = subscribe(
+        config.laserstream_config,
+        config.subscribe_request,
+    );
+
+    println!("Connected to Helius LaserStream");
+
+    tokio::pin!(stream);
+
+    loop {
+        tokio::select! {
+            Some(message) = stream.next() => {
+                match message {
+                    Ok(update) => {
+                        println!("Received tx protobuf; {:?}", update);
+                    }
+                    Err(e) => {
+                        println!("Error receiving message: {:?}", e);
+                    }
+                }
+            }
+            _ = signal::ctrl_c() => {
+                println!("\nReceived Ctrl+C command, shutting down connection to Helius");
+                break;
+            }
+        }
+    }
+
+
+    /*
+    while let Some(message) = stream.next().await {
+        match message {
+            Ok(update) => {
+                println!("Received tx: {:?}", update);
+            }
+            Err(e) => {
+                println!("Error receiving message: {}", e);
+            }
+        }
+    }
+    */
+    
+    Ok(())
 }
