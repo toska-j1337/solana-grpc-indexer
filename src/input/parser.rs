fca20e25616fd0166adc9525110455dcf8d5c1b5
James Herndon <toska.admin@proton.me>
2026-04-17 07:06:06 +0000

    Improved console output while exploring data structures

--- src/input/parser.rs
+++ src/input/parser.rs
@@ -12,19 +12,34 @@ pub fn process_update(update: SubscribeUpdate) {
             println!("Received Transaction: {}", signature);
 
             if let Some(meta) = tx_info.meta {
+
                 println!("    Fee: {} lamports", meta.fee);
+
                 if let Some(compute) = meta.compute_units_consumed {
                     println!("    Compute Units Consumed: {}", compute);
                 }
+                //Parsing token balance changes.
                 if !meta.pre_token_balances.is_empty() || !meta.post_token_balances.is_empty() {
+
                     println!("    Token Balances Changed: ");
+
                     for (pre, post) in meta.pre_token_balances.iter().zip(meta.post_token_balances.iter()) {
-                        if pre.mint == post.mint && pre.ui_token_amount != post.ui_token_amount {
-                            println!("    Mint: {} | Change: {:?} → {:?}",
-                                pre.mint,
-                                pre.ui_token_amount,
-                                post.ui_token_amount
-                            );
+                        
+                        if pre.mint == post.mint {//extracting f64 from UiTokeAmount struct in
+                                                  //pre/post_amount.
+                            let pre_amount = pre.ui_token_amount.as_ref().map(|x| x.ui_amount).unwrap_or(0.0);
+                            let post_amount = post.ui_token_amount.as_ref().map(|x| x.ui_amount).unwrap_or(0.0);
+                            let delta = post_amount - pre_amount;
+                            let delta_abs = delta.abs();
+                            //Abs to filter noise.
+                            if delta_abs > 0.000001 {
+                                println!("    Mint: {} | Owner: {} | {:.6} → {:.6} (delta: {:.6})",
+                                    pre.mint,
+                                    pre.owner,
+                                    pre_amount,
+                                    post_amount,
+                                    delta);
+                            }
                         }
                     }
                 }
