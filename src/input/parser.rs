5b9e45b0bc171e5d3f0f3fe12ff210083b77e3c1
James Herndon <toska.admin@proton.me>
2026-04-17 06:12:06 +0000

    Moved update parsing to module

--- src/input/parser.rs
+++ src/input/parser.rs
@@ -1,2 +1,34 @@
-//parser.rs handles protobuf data, extracting what I need (txid, etc)
+//parser.rs handles protobuf data,
+use helius_laserstream::grpc::{subscribe_update, SubscribeUpdate};
+use bs58;
 
+pub fn process_update(update: SubscribeUpdate) {
+    if let Some (subscribe_update::UpdateOneof::Transaction(tx_update)) = update.update_oneof {
+        
+        if let Some(tx_info) = tx_update.transaction {
+            let signature_bytes = tx_info.signature;
+            let signature = bs58::encode(&signature_bytes).into_string();
+            
+            println!("Received Transaction: {}", signature);
+
+            if let Some(meta) = tx_info.meta {
+                println!("    Fee: {} lamports", meta.fee);
+                if let Some(compute) = meta.compute_units_consumed {
+                    println!("    Compute Units Consumed: {}", compute);
+                }
+                if !meta.pre_token_balances.is_empty() || !meta.post_token_balances.is_empty() {
+                    println!("    Token Balances Changed: ");
+                    for (pre, post) in meta.pre_token_balances.iter().zip(meta.post_token_balances.iter()) {
+                        if pre.mint == post.mint && pre.ui_token_amount != post.ui_token_amount {
+                            println!("    Mint: {} | Change: {:?} → {:?}",
+                                pre.mint,
+                                pre.ui_token_amount,
+                                post.ui_token_amount
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod config;
+mod input;
 
+use crate::input::parser;
 use config::AppConfig;
-use helius_laserstream::{subscribe, grpc::subscribe_update};
+use helius_laserstream::{subscribe};
 use futures_util::StreamExt;
 use tracing_subscriber;
 use tokio::signal;
-use bs58;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -32,36 +33,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             Some(message) = stream.next() => {
                 match message {
                     Ok(update) => {
-                        if let Some(subscribe_update::UpdateOneof::Transaction(tx_update)) = update.update_oneof {
-                            
-                            if let Some(tx_info) = tx_update.transaction {
-                                let signature_bytes = tx_info.signature;
-                                let signature = bs58::encode(&signature_bytes).into_string();
-
-                                println!("Received Transaction: {}", signature);
-
-                                if let Some(meta) = tx_info.meta {
-                                    println!("  Fee: {} lamports", meta.fee);
-                                    if let Some(compute) = meta.compute_units_consumed {
-                                        println!("   Compute Units Consumed: {}", compute);
-                                    }
-                                    if !meta.pre_token_balances.is_empty() || !meta.post_token_balances.is_empty() {
-                                        println!("    Token balances changed: ");//to get Pre/Post
-                                                                                 //token account
-                                                                                 //balances
-                                        for (pre, post) in meta.pre_token_balances.iter().zip(meta.post_token_balances.iter()) {
-                                            if pre.mint == post.mint && pre.ui_token_amount != post.ui_token_amount {
-                                                println!("    Mint: {} | Change: {:?} -> {:?}",
-                                                    pre.mint,
-                                                    pre.ui_token_amount,
-                                                    post.ui_token_amount);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                       // println!("Received tx protobuf; {:?}", update);
+                        //Parsing input below.
+                        parser::process_update(update);
                     }
                     Err(e) => {
                         println!("Error receiving message: {:?}", e);
